2586ff0be45a6d4533187ab388c91fb4523761c0
Asa-to <sethuna03@gmail.com>
2020-07-14 19:40:36 +0900

    バグを修正

--- routes/inputForm.js
+++ routes/inputForm.js
@@ -9,8 +9,8 @@ router.post('/confirm', function(req, res, next){
     var today = new Date();
     var data = {
         year: req.body['year'] ? req.body['year'] : today.getFullYear(),
-        month: req.body['month'] ? req.body['month'] : today.getMonth(),
-        day: req.body['day'] ? req.body['day'] : today.getDay(),
+        month: req.body['month'] ? req.body['month'] : today.getMonth()+1,
+        day: req.body['day'] ? req.body['day'] : today.getDate(),
         payment: req.body['payment'] ? req.body['payment'] : 0,
         expence: req.body['balance'].value === 'expence'
     };
