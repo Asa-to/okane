716f7a7d28e3afe946bfa499a32f12cba6441e35
Asa-to <sethuna03@gmail.com>
2020-07-21 13:28:11 +0900

    すべての日付にyyyymmdd形式のクラスと、英語表記の曜日クラスを振った。ついでに土日は色を変えた

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -13,15 +13,15 @@ const months = {
     11: 'December'
 };
 
-// const weeks = {
-//     0: 'Sunday',
-//     1: 'Monday',
-//     2: 'Tuesday',
-//     3: 'Wednesday',
-//     4: 'Thursday',
-//     5: 'Fryday',
-//     6: 'Saturday'
-// };
+const weeksEN = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Fryday',
+    6: 'Saturday'
+};
 
 const weeks = {
     0: '日',
@@ -55,43 +55,6 @@ const setNextMonth = () => {
     }
 }
 
-
-const createCalendar = () => {
-    const date = new Date(year, month);
-    //tableHtmlにカレンダーのhtmlを追加していく
-    let tableHtml = '';
-    //週を足す
-    tableHtml = '<table><thead><tr>';
-    for(let i = 0; i < 7; i++){
-        tableHtml += `<th>${weeks[i]}</th>`;
-    }
-    tableHtml += '</tr></thead>'
-    //1日の曜日(wday)を求めて、そこから最終日(lastDay)まで日にちを入れていく
-    const firstWday = date.getDay();
-    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-    console.log(lastDay);
-    //週毎に日にちを入れていく
-    let day = 1;
-    while(day < lastDay){
-        tableHtml += '<tr>';
-        for(let wday = 0; wday < 7; wday++ ){
-            if(day == 1 && wday < firstWday || lastDay <= day){
-                tableHtml += '<td></td>';
-            }else{
-                tableHtml += `<td>${day++}</td>`;
-            }
-        }
-        tableHtml += '</tr>';
-    }
-    tableHtml += '</table>'
-    document.getElementById('calendar').innerHTML = tableHtml;
-}
-
-const showCalendar = () => {
-    createCalendar();
-    document.getElementById('thisMonth').innerText = `${year}年${month+1}月`;
-}
-
 document.getElementById('nextMonthButton').addEventListener('click', () => {
     setNextMonth();
     showCalendar();
@@ -101,6 +64,49 @@ document.getElementById('lastMonthButton').addEventListener('click', () => {
     setLastMonth();
     showCalendar();
 });
+
+
+const setDateOption = () => {
+    Array.prototype.forEach.call(document.getElementsByClassName(`${today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate()}`), element => {
+        element.classList.add('today');
+    });
+}
+
+const createCalendar = () => {
+    const date = new Date(year, month);
+    //tableHtmlにカレンダーのhtmlを追加していく
+    let tableHtml = '';
+    //週を足す
+    tableHtml = '<table><thead><tr>';
+    for(let i = 0; i < 7; i++){
+        tableHtml += `<th class="${weeksEN[i]}">${weeks[i]}</th>`;
+    }
+    tableHtml += '</tr></thead>'
+    //1日の曜日(wday)を求めて、そこから最終日(lastDay)まで日にちを入れていく
+    const firstWday = date.getDay();
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    //週毎に日にちを入れていく
+    let day = 1;
+    while(day < lastDay){
+        tableHtml += '<tr>';
+        for(let wday = 0; wday < 7; wday++ ){
+            if(day == 1 && wday < firstWday || lastDay <= day){
+                tableHtml += '<td></td>';
+            }else{
+                tableHtml += `<td class="${year * 10000 + (month + 1) * 100 + day} ${weeksEN[wday]}">${day++}</td>`;
+            }
+        }
+        tableHtml += '</tr>';
+    }
+    tableHtml += '</table>'
+    document.getElementById('calendar').innerHTML = tableHtml;
+    setDateOption();
+}
+
+const showCalendar = () => {
+    createCalendar();
+    document.getElementById('thisMonth').innerText = `${year}年${month+1}月`;
+}
 
 window.onload = () =>{
     showCalendar();
