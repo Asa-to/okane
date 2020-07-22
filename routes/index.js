98782c0070dc78e3ca001b7d25670f6eeb5f9b0e
Asa-to <sethuna03@gmail.com>
2020-07-22 22:26:04 +0900

    ログインページをなくして直接googleログインするようにした

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if(!req.user)res.redirect('/auth/google');
   res.render('index', { 
     user: req.user
   });
