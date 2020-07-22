34c9ca651993c91ad5012429ed8524764b7c8b03
Asa-to <sethuna03@gmail.com>
2020-07-23 01:49:37 +0900

    細かなエラーを修正した

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  if(!req.user)res.redirect('/auth/google');
-  res.render('index', { 
-    user: req.user
-  });
-  console.log(`${req.user.displayName}(${req.user.id})さんがログインしました`);
+router.get('/', function (req, res, next) {
+  if (!req.user) res.redirect('/auth/google');
+  else {
+    res.render('index', {
+      user: req.user
+    });
+    console.log(`${req.user.displayName}(${req.user.id})さんがログインしました`);
+  }
 });
 
 module.exports = router;
